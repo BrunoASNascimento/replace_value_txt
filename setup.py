b71d27fe5a9f061e1931d1f253999f6f1a94787b
BrunoASNascimento <bruno-asn@outlook.com.br>
2020-11-01 12:45:52 -0300

    code test ok

--- setup.py
+++ setup.py
@@ -4,28 +4,27 @@ with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='beaufort_scale',
-    version='1.0.1',
-    author='Bruno Nascimento',
-    author_email='bruno_freddy@hotmail.com',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=['beaufort_scale'],
+    name='replace_value_txt',  # Name library
+    version='1.0.0',  # Version
+    author='Bruno Nascimento',  # Name author
+    author_email='bruno_freddy@hotmail.com',  # Author email
+    long_description=long_description,  # Description read README.md
+    long_description_content_type='text/markdown',  # Type description
+    packages=['replace_value_txt'],
     url='https://github.com/BrunoASN/beaufort-scale',
     project_urls={
-        'Código fonte': 'https://github.com/BrunoASN/beaufort-scale',
-        'Download': 'https://github.com/BrunoASN/beaufort-scale/archive/master.zip'
+        'Código fonte': 'https://github.com/BrunoASNascimento/replace_value_txt',
+        'Download': 'https://github.com/BrunoASNascimento/replace_value_txt/archive/main.zip'
     },
-    license='MIT',
-    keywords=['conversor', 'wind', 'speed', 'beaufort'],
+    license='MIT',  # License name
+    keywords=['replace'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: Portuguese (Brazilian)',
         'Operating System :: OS Independent',
-        'Topic :: Software Development :: Internationalization',
-        'Topic :: Scientific/Engineering :: Physics'
+        'Topic :: Software Development :: Internationalization'
     ],
-    python_requires='>=3.4'
+    python_requires='>=3.6'
 )
